367f1dc7af70f7f717e0b74f34f344575ba07623
ahmetfteke <ateke1@binghamton.edu>
2017-04-28 02:07:27 -0400

    exercise 5

---
+++ 5_exercise.py
@@ -0,0 +1,6 @@
+ # Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+
+ name = input("Your name: ")
+ surname = input("Your surname: ")
+
+ print("Hi, {} {}".format(surname, name))
