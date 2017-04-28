c18b2d7ded94242b8a8fb917913f533d213616a5
ahmetfteke <ateke1@binghamton.edu>
2017-04-28 02:11:04 -0400

    exercise 7

---
+++ 7_exercise.py
@@ -0,0 +1,6 @@
+# Write a Python program to accept a filename from the user and print the extension of that. Go to the editor
+# Sample filename : abc.java 
+# Output : java
+
+file_name = input("Enter filename: ")
+print(filename.split(".")[-1])
